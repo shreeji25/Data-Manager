576e9d65f8757fcacd6041c935b89b422e3f16d8
shreeji25@ <shreejijansari25@gmail.com>
2026-02-26 12:17:28 +0530

    fix database URL for railway

--- database.py
+++ database.py
@@ -5,6 +5,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:25092004@localhost:5432/dataset_portal")
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://", 1)
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
